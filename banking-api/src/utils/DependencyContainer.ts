38c2af42c9ec2ab7eea649492ebb9e3a414bfc61
Danillo Tomaz Parreira <parreira16@gmail.com>
2024-09-16 18:56:25 -0300

    Atualização

--- banking-api/src/utils/DependencyContainer.ts
+++ banking-api/src/utils/DependencyContainer.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const userService = new UserService();
+
 const tokenSecret = process.env.TOKEN_SECRET as string;
 const tokenDuration = process.env.TOKEN_DURATION as string;
+
 export { userService, tokenSecret, tokenDuration }
